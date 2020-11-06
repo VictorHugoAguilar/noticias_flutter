14afce6f34149d7d5bc38d58c86df34a218d9228
victorHugoAguilar <victorhugoaguilar@aol.com>
2020-11-06 13:07:31 +0100

    Cambiando el PageView dependiendo de la opción seleccionada

--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -41,7 +41,10 @@ class _Navegacion extends StatelessWidget {
 class _Paginas extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final navegacionModel = Provider.of<_NavegacionModel>(context);
+
     return PageView(
+      controller: navegacionModel.pageController,
       // physics: BouncingScrollPhysics(),
       physics: NeverScrollableScrollPhysics(),
       children: <Widget>[
@@ -58,11 +61,21 @@ class _Paginas extends StatelessWidget {
 
 class _NavegacionModel with ChangeNotifier {
   int _paginaActual = 0;
+  PageController _pageController = new PageController();
 
   int get paginaActual => this._paginaActual;
 
   set paginaActual(int valor) {
     this._paginaActual = valor;
+
+    _pageController.animateToPage(
+      valor,
+      duration: Duration(milliseconds: 250),
+      curve: Curves.easeIn,
+    );
+
     notifyListeners();
   }
+
+  PageController get pageController => this._pageController;
 }
